dd84f72cd65f91d790c157ecddbd685cf4f61004
luiza.puccinelli <luiza.puccinelli06@gmail.com>
2022-08-15 12:24:07 -0300

    full home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,15 @@ export default function Home() {
   return (
     <div className="bg-gray-50 bg-home bg-no-repeat bg-cover flex flex-col justify-between min-h-screen">
       <Header />
-      <div className="relative flex-grow">
+      <div className="h-[calc(100vh_-_96px)]">
         <Notification />
-        <PageAnimation>
-          <HomeHero />
-        </PageAnimation>
+        <div className="h-full flex flex-col justify-between">
+          <PageAnimation>
+            <HomeHero />
+          </PageAnimation>
+          <Footer />
+        </div>
       </div>
-      <Footer />
     </div>
   )
 }
